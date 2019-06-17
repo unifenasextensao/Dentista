4733b252b91f3caf04a5a4b7929953c1ff791b17
Vinícius <viniciusrg10@msn.com>
2019-06-17 14:50:09 -0300

    Inicio

---
+++ Dentista/Dentista/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dentista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
